894e6d5975cc4b7418499c62d4b474fdb0444ee7
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2021-10-18 21:36:45 -0300

    Iniciando o projeto

---
+++ ExercicioPropostoEnumComposicao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioPropostoEnumComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
