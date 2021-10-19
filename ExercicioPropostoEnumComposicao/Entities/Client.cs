a36b17dd1ad40d3f68e203e657205bf09791fd62
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2021-10-18 21:42:41 -0300

    Create entities 'Client' and 'Product'

---
+++ ExercicioPropostoEnumComposicao/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoEnumComposicao.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+        public Client()
+        {
+
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.BirthDate = birthDate;
+        }
+    }
+}
---
+++ ExercicioPropostoEnumComposicao/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoEnumComposicao.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
