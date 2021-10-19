e73a5f0eb9973c3f3fe4a5d485cb3bac6dab0e63
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2021-10-18 22:25:48 -0300

    Create entitie Order and OrderItem

---
+++ ExercicioPropostoEnumComposicao/Entities/Order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExercicioPropostoEnumComposicao.Enums;
+
+namespace ExercicioPropostoEnumComposicao.Entities
+{
+    class Order
+    {
+        public DateTime Momment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime momment, OrderStatus status)
+        {
+            Momment = momment;
+            Status = status;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            this.Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            this.Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+
+            foreach (OrderItem item in this.Items)
+            {
+                total += item.Price;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ ExercicioPropostoEnumComposicao/Entities/OrderItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoEnumComposicao.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public Product Produc { get; set; }
+
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            this.Quantity = quantity;
+            this.Price = price;
+            this.Produc = product;
+        }
+
+        public double subTotal()
+        {
+            return this.Quantity * this.Price;
+        }
+    }
+}
